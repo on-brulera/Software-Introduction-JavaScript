01fe70ff4de32ef73fa02f541211db92dd5a7f2f
on-brulera <luishenryramirezmontalvo746@gmail.com>
2022-08-12 18:10:20 -0500

    Se añade pagina web con definiciones de const para variables primitivos y compuestos

---
+++ 01-Tipos-Datos/1.2-Constantes/constantes.js
@@ -0,0 +1,19 @@
+const PI = 3.1419;
+console.log("Constante: ", PI);
+
+//crear objeto
+let persona = {
+    nombre: "Henry",
+    edad: 20
+}
+console.log(persona)
+persona.correo = "henry@gmail.com";
+console.log(persona);
+
+//crear lista
+let colores = [
+    "blanco", "azul", "rojo"
+]
+console.log(colores);
+colores.push("verde");
+console.log(colores);
